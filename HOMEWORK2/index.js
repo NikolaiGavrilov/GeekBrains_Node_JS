23485396add12ba275df6ec73be4266a7451de67
NikolaiGavrilov <nikolaisibir@mail.ru>
2024-07-13 14:18:44 +0300

    Completed homework 2

---
+++ HOMEWORK2/index.js
@@ -0,0 +1,7 @@
+// Обращаюсь к созданному мной же package https://www.npmjs.com/package/drawfigures после его инсталляции
+const df = require('drawfigures');
+//Проверяю функционал
+df.drawTriangle(4);
+df.drawTriangleSym(7,"e");
+df.drawSquareSym(5,"&");
+df.drawSquare(5);
